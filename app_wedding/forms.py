e6f9ba6bb3f8e5feabd9261bda9417720fffe98e
benito <gon.beni@gmail.com>
2020-07-16 13:19:41 +0200

    Updated form in confirmation

--- app_wedding/forms.py
+++ app_wedding/forms.py
@@ -8,7 +8,7 @@ class ContactForm(forms.Form):
     name = forms.CharField(error_messages={'required': 'Por favor, indique su nombre'})
     bus = forms.CharField(error_messages={'required': 'Por favor, indique indique si acudirá en autobús'})
     guests = forms.CharField(error_messages={'required': 'Por favor, indique el número total de invitados'})
-    message = forms.CharField(error_messages={'required': 'Indique el nombre de los invitados y cualquier tipo de alergia.'})
+    message = forms.Textarea()
 
     def send_email(self):
         with get_connection(
